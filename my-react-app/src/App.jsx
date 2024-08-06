d06eb4b52c5f786969568d7a950660b2cd457309
Chunfeng Liu <cliu3@rocketsoftware.com>
2024-08-06 14:09:40 +0800

    feat: Split to a component

--- my-react-app/src/App.jsx
+++ my-react-app/src/App.jsx
@@ -1,58 +1,63 @@
-import { useState } from 'react'
+import { useState } from "react";
 
 const welcome = {
-  greeting: 'Hi',
-  title: 'React'
-}
+  greeting: "Hi",
+  title: "React",
+};
 
 function getTitle(title) {
   return title;
 }
 
 const list = [
-   {
-    title: 'React',
-    url: 'https://reactjs.org/',
-    author: 'Jordan Walke',
+  {
+    title: "React",
+    url: "https://reactjs.org/",
+    author: "Jordan Walke",
     num_comments: 3,
     points: 4,
     objectID: 0,
   },
   {
-    title: 'Redux',
-    url: 'https://redux.js.org/',
-    author: 'Dan Abramov, Andrew Clark',
+    title: "Redux",
+    url: "https://redux.js.org/",
+    author: "Dan Abramov, Andrew Clark",
     num_comments: 2,
     points: 5,
     objectID: 1,
   },
-]
+];
+
+function List() {
+  return list.map(function (item) {
+    return (
+      <div key={item.objectID}>
+        <span>
+          <a href={item.url}>{item.title}</a>
+        </span>
+        <span>{item.author}</span>
+        <span>{item.num_comments}</span>
+        <span>{item.points}</span>
+      </div>
+    );
+  });
+}
 
 function App() {
-
   return (
     <div>
-      <h1>{welcome.greeting}, {getTitle(welcome.title)}</h1>
-      
+      <h1>
+        {welcome.greeting}, {getTitle(welcome.title)}
+      </h1>
+
       <label htmlFor="search">Search:</label>
       <input id="search" type="text" />
 
-      <div>
-        {list.map(function(item) {
-          return (
-            <div key={item.objectID}>
-              <span>
-                <a href={item.url}>{item.title}</a>
-              </span>
-              <span>{item.author}</span>
-              <span>{item.num_comments}</span>
-              <span>{item.points}</span>
-            </div>
-          )
-        })}
-      </div>
+      <hr />
+
+      <List />
     </div>
- )
+  );
 }
 
-export default App
+export default App;
