7f20a55203db1fc198294f9bd74980b6f7b3ab77
Chunfeng Liu <cliu3@rocketsoftware.com>
2024-08-26 09:09:28 +0800

    feat: Update to more standard code

--- 002-deep-dive-hooks/src/pages/FeedbackPage.jsx
+++ 002-deep-dive-hooks/src/pages/FeedbackPage.jsx
@@ -1,67 +1,117 @@
-import { useEffect, useMemo, useRef, useState } from "react"
+import { useMemo, useState } from "react";
 
 function FeedbackPage() {
-  const [name, setName] = useState('')
-  const [email, setEmail] = useState('')
-  const [feedback, setFeedback] = useState('')
-  const [isAgree, setIsAgree] = useState(false)
-  const [isSubmit, setIsSubmit] = useState(false)
+  // const [isAgree, setIsAgree] = useState(false);
+  const [isSubmit, setIsSubmit] = useState(false);
+  const [formData, setFormData] = useState({
+    name: "",
+    email: "",
+    feedback: "",
+    agreement: false,
+  });
 
   const feedbackLen = useMemo(() => {
-    return feedback.length
-  }, [feedback])
+    return formData.feedback.length;
+  }, [formData.feedback]);
+
+  const handleChange = (e) => {
+    const { value, name, type, checked } = e.target;
+
+    setFormData((preState) => ({
+      ...preState,
+      [name]: type === "checkout" ? checked : value,
+    }));
+  };
 
   const handleSubmit = (e) => {
-    console.log(e)
+    console.log(e);
     e.preventDefault();
-    setIsSubmit(true)
+    setIsSubmit(true);
     handleReset();
     setTimeout(() => {
-      setIsSubmit(false)
+      setIsSubmit(false);
     }, 2000);
-  }
+  };
 
   const handleReset = () => {
-    setName('')
-    setEmail('')
-    setFeedback('')
-  }
+    setFormData({
+      name: "",
+      email: "",
+      feedback: "",
+    });
+  };
 
   return (
     <div className="max-w-[400px] m-auto">
       <form onSubmit={handleSubmit}>
         <div className="flex flex-col">
           <label className="font-bold">name:</label>
-          <input required value={name} onChange={(e) => { setName(e.target.value) }} placeholder="Write your name" className="border border-gray-200 p-2 rounded-md" />
+          <input
+            required
+            value={formData.name}
+            name="name"
+            onChange={handleChange}
+            placeholder="Write your name"
+            className="border border-gray-200 p-2 rounded-md"
+          />
         </div>
         <div className="flex flex-col">
           <label className="font-bold">email:</label>
-          <input required value={email} onChange={(e) => { setEmail(e.target.value) }} type="email" placeholder="Write your email address" className="border border-gray-200 p-2 rounded-md" />
+          <input
+            required
+            value={formData.email}
+            name="email"
+            onChange={handleChange}
+            type="email"
+            placeholder="Write your email address"
+            className="border border-gray-200 p-2 rounded-md"
+          />
         </div>
         <div className="flex flex-col">
           <label className="font-bold">feedback:</label>
-          <textarea value={feedback} onChange={(e) => { setFeedback(e.target.value) }} cols="40" rows="4" placeholder="Please provide your valuable feedback" className="border border-gray-200 p-2 rounded-md" type="textarea" ></textarea>
+          <textarea
+            value={formData.feedback}
+            name="feedback"
+            onChange={handleChange}
+            cols="40"
+            rows="4"
+            placeholder="Please provide your valuable feedback"
+            className="border border-gray-200 p-2 rounded-md"
+            type="textarea"
+          ></textarea>
         </div>
         <div className="mt-4">
           Character Count: <span>{feedbackLen}</span>
         </div>
         <div className="mt-5">
           <label className="flex gap-2 font-bold " htmlFor="agree">
-            <input id="agree" type="checkbox" checked={isAgree} onChange={() => { setIsAgree(agree => !agree) }} />
+            <input
+              id="agree"
+              type="checkbox"
+              name="agreement"
+              checked={formData.agreement}
+              onChange={handleChange}
+            />
             <span>I agree to the terms and conditions</span>
           </label>
         </div>
         <div className="flex gap-1 mt-4">
-          <button disabled={!isAgree} type="submit">Submit</button>
-          <button type="button" onClick={handleReset}>Reset</button>
+          <button disabled={!formData.agreement} type="submit">
+            Submit
+          </button>
+          <button type="button" onClick={handleReset}>
+            Reset
+          </button>
         </div>
 
-        {isSubmit && (<div className="mt-4">
-          <p>Form submit successfully!</p>
-        </div>)}
+        {isSubmit && (
+          <div className="mt-4">
+            <p>Form submit successfully!</p>
+          </div>
+        )}
       </form>
     </div>
-  )
+  );
 }
 
-export default FeedbackPage
+export default FeedbackPage;
